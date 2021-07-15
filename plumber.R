41d8796302edc9a2ed5ce4161db6b5677f7a63b7
josiahparry <josiah.parry@gmail.com>
2021-07-15 13:38:45 -0400

    things and stuff

---
+++ plumber.R
@@ -0,0 +1,27 @@
+library(plumber)
+library(tidyverse)
+library(tidymodels)
+
+penguins_model <- readr::read_rds("model/penguin-model.rds")
+
+#* @apiTitle Male or Female Penguin?
+
+#* Return the probability of male or female based on penguin body characteristics and species
+
+#* @param species Species of penguin (Adelie, Gentoo, Chinstrap)
+#* @param bill_length_mm:numeric Bill length in millimeters (eg. 38.6)
+#* @param bill_depth_mm:numeric Bill depth in millimeters (eg. 17.4)
+#* @param flipper_length_mm:numeric Flipper length in millimeters (eg. 180)
+#* @param body_mass_g:numeric Body mass in grams (eg. 3400)
+#* @get /pred
+function(species, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g){
+  pred_tibble <- tibble(species = species, 
+                  bill_length_mm = as.numeric(bill_length_mm),
+                  bill_depth_mm = as.numeric(bill_depth_mm),
+                  flipper_length_mm = as.numeric(flipper_length_mm),
+                  body_mass_g = as.numeric(body_mass_g))
+  
+  predict(penguins_model, pred_tibble, type = "prob")
+}
+
+
